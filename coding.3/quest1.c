907fccce93ce02bcf8526a97de21e141262b01dd
heigleyj <heigleyj@csc4252022.ssdt.berea.edu>
2023-09-10 17:17:11 -0400

    Answered Questions 1 and 2

---
+++ coding.3/quest1.c
@@ -0,0 +1,32 @@
+/*************************
+Name: Josh Heigley
+Question 1: What value is the variable in the child process?
+Answer: The value will stay the same as it was unless it is changed after the fork, and will be the same as it is in the parent process unless it is changed in a section of program
+that the parent process cannot enter.
+Question 2: What happens to the variable when both the child and parent change the value of x?
+Answer: The variable changes independantly of the parent and child when each change it to a different value in their own section of the program. It essentially becomes two different
+variables that started off as the same.
+**************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+int x = 100;
+printf("Before Fork (value:%d)\n", (int) x);
+int rc = fork();
+if (rc < 0) {
+// fork failed
+fprintf(stderr, "fork failed\n");
+exit(1);
+} else if (rc == 0) {
+// child (new process)
+x += 50;
+printf("child (value:%d)\n", (int) x);
+} else {
+// parent goes down this path (main)
+x += 100;
+printf("parent (value:%d)\n", (int) x);
+}
+return 0;
+}
---
+++ coding.3/quest2.c
@@ -0,0 +1,31 @@
+/*************************
+Name: Josh Heigley
+Question 1: Can both the child and parent access the file descriptor returned by open()?
+Answer: Yes, both processes are able th acces the file descriptor and write or read from the file.
+Question 2: What happens when they are writing to the file concurrently, i.e., at the same time?
+Answer: From what I have seen, one process will write everything onto the file, and then the next process will write everything that it needs.
+**************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+int desc = open("file.txt", O_WRONLY);
+int rc = fork();
+if (rc < 0) {
+// fork failed
+fprintf(stderr, "fork failed\n");
+exit(1);
+} else if (rc == 0) {
+// child (new process)
+write(desc, "hello I am child method\n", 24);
+printf("Child wrote to file\n");
+} else {
+// parent goes down this path (main)
+write(desc, "hello I am parent method\n", 25);
+printf("Parent wrote to file\n");
+}
+close(desc);
+return 0;
+}
